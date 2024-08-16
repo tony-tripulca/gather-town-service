4ace301a318cde7d832a97750ebe93603ddeec14
tony-tripulca <tony.tripulca@gmail.com>
2024-08-17 01:02:03 +0800

    First commit

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -1,8 +1,8 @@
 module.exports = {
   apps: [
     {
-      name: "LearnerService.1.2",
-      namespace: "skhillz-learner-service",
+      name: "GatherTownService",
+      namespace: "gather-town-service",
       script: "./src/index.js",
       watch: ["./src", "./src/*.js"],
       output: "./logs/out.log",
